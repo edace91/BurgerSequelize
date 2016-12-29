b2c7c887f1051129d19346810f98aed37de6c3dd
edace91 <eduace91@gmail.com>
2016-12-28 19:23:09 -0600

    setting up sequelize

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -4,15 +4,14 @@ and the logic of each route.
 */
 var express = require('express');
 var router = express.Router();
-var burger = require('../models/burger.js');
-
+var models = require('../models');
 
 router.get('/', function (req, res) {
 	res.redirect('/burgers');
 });
 
 router.get('/burgers', function (req, res) {
-	burger.all(function (data) {
+	models.Burgers.all(function (data) {
 		var hbsObject = { burgers: data };
 		console.log(hbsObject);
 		res.render('index', hbsObject);
@@ -20,7 +19,7 @@ router.get('/burgers', function (req, res) {
 });
 
 router.post('/burgers/create', function (req, res) {
-	burger.create(['burger_name', 'devoured'], [req.body.name, false], function () {
+	models.Burgers.create(['burger_name', 'devoured'], [req.body.name, false], function () {
 		res.redirect('/burgers');
 	});
 });
@@ -31,7 +30,7 @@ router.put('/burgers/update/:id', function (req, res) {
 	console.log('condition', condition);
 	console.log(req.body)
 
-	burger.update({ devoured: req.body.devoured }, condition, function () {
+	models.Burgers.update({ devoured: req.body.devoured }, condition, function () {
 		res.redirect('/burgers');
 	});
 });
---
+++ migrations/20161229011027-burgers.js
@@ -0,0 +1,32 @@
+'use strict';
+
+module.exports = {
+  up: function (queryInterface, Sequelize) {
+    /*
+      Add altering commands here.
+    */
+
+    return models.Burgers.bulkCreate(
+        [
+          {burger_name: "Habanero Burger"},
+          {burger_name: "Fresh Avacado Burger"},
+          {burger_name: "Ahi Tuna"},
+          {burger_name: "Double with Cheese"},
+          {burger_name: "Chicken Poblano"}
+        ]
+     )
+  },
+
+  down: function (queryInterface, Sequelize) {
+    /*
+      Add reverting commands here.
+    */
+    return models.Burgers.destroy({where: {burger_name:[
+      "Habanero Burger",
+      "Fresh Avacado Burger",
+      "Ahi Tuna",
+      "Double with Cheese",
+      "Chicken Poblano"
+    ]}})
+  }
+};
